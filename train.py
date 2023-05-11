12f914fbb5f0c03a22e1d306e8c28b88ec127698
Kostas Triaridis <kost.tri@gmail.com>
2023-05-12 01:17:34 +0300

    base training loop

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import torch
 from tqdm import tqdm
 
 from torch.optim import SGD
@@ -24,14 +25,18 @@ train_idx, val_idx = train_test_split(np.arange(len(dataset)),
 train_dataset = Subset(dataset, train_idx)
 validation_dataset = Subset(dataset, val_idx)
 STEPS = len(train_dataset) // cfg.BATCH_SIZE
-
+VAL_STEPS = len(validation_dataset) // cfg.BATCH_SIZE
 train_loader = DataLoader(train_dataset,
                           batch_size=cfg.BATCH_SIZE,
-                          shuffle=True)
+                          shuffle=True,
+                          num_workers=4,
+                          pin_memory=True)
 
 val_loader = DataLoader(validation_dataset,
                         batch_size=cfg.BATCH_SIZE,
-                        shuffle=True)
+                        shuffle=True,
+                        num_workers=4,
+                        pin_memory=True)
 
 model = MMNet()
 model.cuda()
@@ -41,30 +46,57 @@ optimizer = SGD(model.parameters(), cfg.LEARNING_RATE, momentum=0.9)
 scheduler = StepLR(optimizer, step_size=2, gamma=.7)
 
 epoch_losses = []
+epoch_val_losses = []
+def train():
+    for epoch in range(cfg.NUM_EPOCHS):
+        pbar = tqdm(train_loader, desc='Training Epoch {}/{}'.format(epoch, cfg.NUM_EPOCHS), unit='steps')
+        model.train()
+        cum_loss = 0
+        for step, inp in enumerate(pbar):
 
-for epoch in range(cfg.NUM_EPOCHS):
-    pbar = tqdm(train_loader, desc='Training Epoch {}/{}'.format(epoch, cfg.NUM_EPOCHS), unit='steps')
-    model.train()
-    cum_loss = 0
-    for step, inp in enumerate(pbar):
+            optimizer.zero_grad()
 
-        optimizer.zero_grad()
+            images, cumulants, _, labels = inp
 
-        images, cumulants, _, labels = inp
+            images = images.cuda()
+            cumulants = cumulants.cuda().float()
+            labels = labels.cuda()
 
-        images = images.cuda()
-        cumulants = cumulants.cuda().float()
-        labels = labels.cuda()
+            logits = model(cumulants, images)
 
-        logits = model(cumulants, images)
+            batch_loss = CrossEntropyLoss()(logits, labels)
+            batch_loss.backward()
+            optimizer.step()
+            cum_loss += batch_loss.item()
 
-        batch_loss = CrossEntropyLoss()(logits, labels)
-        batch_loss.backward()
-        optimizer.step()
-        cum_loss += batch_loss.item()
+            pbar.set_postfix({"last_loss": batch_loss.item()})
 
-        pbar.set_postfix({"last_loss": batch_loss.item()})
+        epoch_loss = cum_loss / STEPS
+        epoch_losses.append(epoch_loss)
+        scheduler.step()
 
-    epoch_loss = cum_loss / STEPS
-    epoch_losses.append(epoch_loss)
-    scheduler.step()
+        pbar = tqdm(val_loader, desc='Validating Epoch {}/{}'.format(epoch, cfg.NUM_EPOCHS), unit='steps')
+        model.eval()
+        cum_loss = 0
+        for step, inp in enumerate(pbar):
+            with torch.no_grad():
+                images, cumulants, _, labels = inp
+
+                images = images.cuda()
+                cumulants = cumulants.cuda().float()
+                labels = labels.cuda()
+
+                logits = model(cumulants, images)
+
+            batch_loss = CrossEntropyLoss()(logits, labels)
+
+            cum_loss += batch_loss.item()
+            pbar.set_postfix({"last_loss": batch_loss.item()})
+
+        epoch_val_loss = cum_loss / VAL_STEPS
+        epoch_val_losses.append(epoch_val_loss)
+        if epoch_val_loss <= min(epoch_val_losses):
+            torch.save(model.state_dict(), "./ckpt/" + cfg.MODEL_NAME)
+
+if __name__ == "__main__":
+    train()
